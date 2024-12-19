eb172c5356f601de91d2913d14e1ace153634d82
Jay9959 <dobariyajay9959@gmail.com>
2024-12-19 16:13:25 +0530

    Exam

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+const App = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+import App from './App.jsx'
+import Header1 from './componet/Header-1.jsx';
+import Header2 from './componet/Header-2.jsx';
+import Style1 from './componet/Style1.jsx';
+import Style2 from './componet/Style2.jsx';
+import Style3 from './componet/Style3.jsx';
+import Style4 from './componet/Style4.jsx';
+import Footer from './componet/Footer.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <Header1 />
+    <Header2 />
+    <Style1 />
+    <Style2 />
+    <Style3 />
+    <Style4 />
+    <Footer />
+  </StrictMode>,
+)
+
+
+
+
+// https://maraviyainfotech.com/projects/carrot/carrot-v2/carrot-html/elements-products.html React.js Vite React-Bootstrap in full link code
+// https://maraviyainfotech.com/projects/carrot/carrot-v2/carrot-html/elements-products.html
+// https://bolt.new/
+// https://ayush-097.github.io/ayush_patel/#home
+// portfolio-specific tools
+// LESifBnrc%C8%a3
